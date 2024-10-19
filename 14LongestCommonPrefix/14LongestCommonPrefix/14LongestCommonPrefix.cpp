138e058aa42184ceb16615dab7b1d940f08ce29d
Osama <osama.qalam@hotmail.com>
2024-10-19 17:25:25 -0400

    solved 14 and 27

---
+++ 14LongestCommonPrefix/14LongestCommonPrefix/14LongestCommonPrefix.cpp
@@ -0,0 +1,51 @@
+﻿// 14LongestCommonPrefix.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+ 
+#include <string> 
+#include <vector> 
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        string result = "";
+        bool breakflag = false;
+
+
+        for (int charIndex = 0; charIndex < strs.at(0).length(); charIndex++)
+        {
+            int strIndex = 0;
+            while (strIndex < strs.size())
+            {
+                if (strs.at(strIndex++)[charIndex] != strs.at(0)[charIndex])
+                {
+                    breakflag = true;
+                    break;
+                }
+            }
+
+            if (breakflag)
+                break;
+
+            result += strs.at(0)[charIndex];
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    vector<string> case1 = { "flower","flow","flight" };
+    vector<string> case2 = { "a" };
+
+    Solution* obj = new Solution;
+    
+    std::cout << obj->longestCommonPrefix(case2);
+}
+
+
+
---
+++ 27RemoveElement/27RemoveElement/27RemoveElement.cpp
@@ -0,0 +1,54 @@
+﻿// 27RemoveElement.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int n = nums.size();
+        int k = n;
+
+        if (n == 0)
+            return 0;
+        
+        for (int i = 0; i < min(n,k)-1; i++)
+        {
+            if (nums.at(i) == val)
+            {
+                nums.at(i) = nums.at(k - 1); // Copy last element to the start
+                k--; i--;
+            }
+        }
+
+        //check last element
+        if (nums.at(k-1) == val)
+            k--;
+
+        return k;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    vector<int> case1 = { 3,2,2,3 };
+    vector<int> case2 = { 0,1,2,2,3,0,4,2 };
+    vector<int> case3 = { 1,1 };
+
+    int val1 = 3;
+    int val2 = 2;
+    int val3 = 1;
+
+
+    int k = obj.removeElement(case3, val3);
+    for (int i : case3)
+        cout << i << ",";
+
+    cout << endl << k;
+}
+
